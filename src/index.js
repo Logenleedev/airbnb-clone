ecfefe604400fba603330d5f1cf21ccd755084ee
Logen <47022821+Logenleedev@users.noreply.github.com>
2022-07-05 20:58:09 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data"
+import "./index.css"
+
+
+
+function App() {
+            // <Hero />
+
+    const card = data.map(item => {
+      return (
+          <div>
+             
+              <Card 
+                  key={item.id}
+                  img={item.coverImg}
+                  rating={item.stats.rating}
+                  reviewCount={item.stats.reviewCount}
+                  location={item.location}
+                  title={item.title}
+                  price={item.price}
+                  openspots={item.openSpots}
+              />
+          </div>
+      )
+    })
+    return(
+      <div>
+        
+        <Navbar />
+        <Hero />
+        <section className="cards-list">
+                {card}
+        </section>
+      </div>
+    
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
